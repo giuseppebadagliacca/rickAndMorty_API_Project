29b1986a10e15ce2b563bfe35484eda2c9c2382f
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-03 13:47:52 -0400

    6/3/22 changes

--- app.js
+++ app.js
@@ -2,21 +2,65 @@
 
 const displayDOM = document.querySelector('#display');
 
-fetch(`https://rickandmortyapi.com/api/character/98`)
+function getRandomChar(){
+  const randNum = Math.floor(Math.random()*826)
+  apiRequest(randNum)
+}
+
+
+function apiRequest(num){
+  fetch(`https://rickandmortyapi.com/api/character/${num}`)
 .then(res=> res.json()) 
 .then(data => {
-    console.log(data)
-    // console.log(data.name)
-    // console.log(data.image)
-    // console.log(data.species)
-    console.log(data.location.name)
     displayDOM.innerHTML = `
-    <h2>${data.name}</h2>
-    <h3>${data.species}</h3>
-    <h4>${data.location.name}</h4>
-    <img src="${data.image}" alt="Morty not found!">
-    `
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${data.image}" alt="Morty not found!">
+  <div class="card-body text-center">
+    <h6>Name:</h6>
+    <h2 class="card-title">${data.name}</h2>
+    <h6>Species:</h6>
+    <h3 class="card-title">${data.species}</h3>
+    <h6>Location:</h6>
+    <h5 class="card-title">${data.location.name}</h5>
+  </div>
+</div>
+<div class = "center pt-3">
+<a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another Character</a>
+</div>
+`
 })
 .catch(err=>{
 
 })
+}
+
+
+
+
+fetch(`https://rickandmortyapi.com/api/character/8`)
+.then(res=> res.json()) 
+.then(data => {
+    displayDOM.innerHTML = `
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${data.image}" alt="Morty not found!">
+  <div class="card-body text-center">
+    <h6>Name:</h6>
+    <h2 class="card-title">${data.name}</h2>
+    <h6>Species:</h6>
+    <h3 class="card-title">${data.species}</h3>
+    <h6>Location:</h6>
+    <h5 class="card-title">${data.location.name}</h5>
+  </div>
+</div>
+<div class = "center pt-3">
+<a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another Character</a>
+</div>
+`
+})
+.catch(err=>{
+
+})
+
+
+
+

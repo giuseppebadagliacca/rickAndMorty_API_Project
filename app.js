bd86600adff1c686391c8b5640a266de6fab8382
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-05 10:23:43 -0400

    6.5.22 changes

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-//https://rickandmortyapi.com/api/character/2  - 826 chars
-
-const displayDOM = document.querySelector('#display');
+const displayDOM = document.querySelector('#display')
 
 function getRandomChar(){
   const randNum = Math.floor(Math.random()*826)
@@ -9,7 +7,7 @@ function getRandomChar(){
 
 
 function apiRequest(num){
-  fetch(`https://rickandmortyapi.com/api/character/${num}`)
+fetch(`https://rickandmortyapi.com/api/character/${num}`)
 .then(res=> res.json()) 
 .then(data => {
     displayDOM.innerHTML = `
@@ -22,42 +20,19 @@ function apiRequest(num){
     <h3 class="card-title">${data.species}</h3>
     <h6>Location:</h6>
     <h5 class="card-title">${data.location.name}</h5>
+    <h5 class="card-title">${data.id}</h5>
   </div>
 </div>
 <div class = "center pt-3">
-<a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another Character</a>
+<a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another *burp* Character</a>
 </div>
 `
 })
 .catch(err=>{
-
+  displayDOM.innerHTML = `
+  <h1 class="text-center">Wubba Lubba Dub Dub! Seems that *burp* something went wrong, *burp* Morty!</h1>
+      <h2 class="text-center">Select a Rick or a *burp* Morty,</h2>
+  `
 })
 }
-
-
-
-fetch(`https://rickandmortyapi.com/api/character/8`)
-.then(res=> res.json()) 
-.then(data => {
-    displayDOM.innerHTML = `
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${data.image}" alt="Morty not found!">
-  <div class="card-body text-center">
-    <h6>Name:</h6>
-    <h2 class="card-title">${data.name}</h2>
-    <h6>Species:</h6>
-    <h3 class="card-title">${data.species}</h3>
-    <h6>Location:</h6>
-    <h5 class="card-title">${data.location.name}</h5>
-  </div>
-</div>
-<div class = "center pt-3">
-<a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another Character</a>
-</div>
-`
-})
-.catch(err=>{
-
-})
-
 

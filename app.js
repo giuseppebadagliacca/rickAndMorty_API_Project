249e3b3e9e2605e2cfc2c6e305060c85c5099222
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-07-14 08:36:25 -0400

    fixed some issues with card display for chars. 7/14/22

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const displayDOM = document.querySelector('#display')
 
-function getRandomChar(){
-  const randNum = Math.floor(Math.random()*826)
+function getRandomChar() {
+  const randNum = Math.floor(Math.random() * 826)
   apiRequest(randNum)
 }
 
 
-function apiRequest(num){
-fetch(`https://rickandmortyapi.com/api/character/${num}`)
-.then(res=> res.json()) 
-.then(data => {
-    displayDOM.innerHTML = `
+function apiRequest(num) {
+  fetch(`https://rickandmortyapi.com/api/character/${num}`)
+    .then(res => res.json())
+    .then(data => {
+      displayDOM.innerHTML = `
     <div class="card" style="width: 18rem;">
   <img class="card-img-top" src="${data.image}" alt="Morty not found!">
   <div class="card-body text-center">
@@ -20,19 +20,18 @@ fetch(`https://rickandmortyapi.com/api/character/${num}`)
     <h3 class="card-title">${data.species}</h3>
     <h6>Location:</h6>
     <h5 class="card-title">${data.location.name}</h5>
-    <h5 class="card-title">${data.id}</h5>
   </div>
 </div>
 <div class = "center pt-3">
 <a href="#" class="btn btn-success" onClick = "getRandomChar()" >Find Another *burp* Character</a>
 </div>
 `
-})
-.catch(err=>{
-  displayDOM.innerHTML = `
+    })
+    .catch(err => {
+      displayDOM.innerHTML = `
   <h1 class="text-center">Wubba Lubba Dub Dub! Seems that *burp* something went wrong, *burp* Morty!</h1>
       <h2 class="text-center">Select a Rick or a *burp* Morty,</h2>
   `
-})
+    })
 }
 
